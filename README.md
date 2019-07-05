b09f0c7346f1555537614ee8bdc93e87e2a1c82e
Kevin Couture <kcouture939@gmail.com>
2019-07-05 13:57:15 -0700

    Lab3 checkpoint

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # jquery-selectors-and-events 
 
-# Feature 1
+## Feature 1
 Number and name of feature: 1 Display images
 
 Estimate of time needed to complete: 1:15
@@ -11,7 +11,7 @@ Finish time: 10:22 AM
 
 Actual time needed to complete: 1:02
 
-# Feature 2
+## Feature 2
 Number and name of feature: 2 Filter images
 
 Estimate of time needed to complete: 1:15
@@ -22,7 +22,7 @@ Finish time: 11:00 AM
 
 Actual time needed to complete: 0:45
 
-# Feature 3
+## Feature 3
 Number and name of feature: 3 Style the application
 
 Estimate of time needed to complete: 1:15
@@ -32,3 +32,50 @@ Start time: 11:15 AM
 Finish time: 12:30 PM
 
 Actual time needed to complete: 1:15
+
+
+# Lab 03: Flexbox and Templating
+
+## Feature 1
+Number and name of feature: #1 Pagination
+
+Estimate of time needed to complete: 0:50
+
+Start time: 12:15 PM
+
+Finish time: 1:30 PM
+
+Actual time needed to complete: 1:15
+
+## Feature 2
+Number and name of feature: #2 Templating
+
+Estimate of time needed to complete: 0:50
+
+Start time: 1:40 PM
+
+Finish time: 1:53 PM
+
+Actual time needed to complete: 0:13 
+
+## Feature 3
+Number and name of feature: #3 Styling with flexbox
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+## Feature 4
+Number and name of feature: #4 Sort the images
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // Global Variables
-let allImages = [];
-let allKeyWords = [];
+let allImagesPg1 = [];
+let allImagesPg2 = [];
+let allKeyWordsPg1 = [];
+let allKeyWordsPg2 = [];
 
 // Constructor for image objects
 // --------------------------------------------------
-const ImgObj = function(image_url, title, description, keyword, horns) {
+const ImgObj = function(image_url, title, description, keyword, horns, allImages) {
   this.image_url = image_url;
   this.title = title;
   this.description = description;
@@ -17,34 +19,39 @@ const ImgObj = function(image_url, title, description, keyword, horns) {
 
 // Read the JSON file from local directory
 // --------------------------------------------------
-function readJSON(filePath, fileType){
+function readJSON(filePath, fileType, allImages, allKeyWords){
   $.get(filePath, fileType).then(results => {
     results.forEach(img => {
       // Instantiate new object using ImgObj constructor
-      new ImgObj(img.image_url, img.title, img.description, img.keyword, img.horns);
+      new ImgObj(img.image_url, img.title, img.description, img.keyword, img.horns, allImages);
 
       // If img.keyword does not exists in allKeyWords, push it into allKeyWords.
       if(!allKeyWords.includes(img.keyword)) {
         allKeyWords.push(img.keyword);
       }
     });
-
-    // Render all of the images
-    allImages.forEach(img => {
-      renderWithJquery(img);
-    });
-
-    // Always hide the section template element
-    $('#photo-template').hide();
-
-    // Poplulate dropdown with keywords
-    populateDropDown();
+    showPage(allImages, allKeyWords);
   });
+}
+
+function showPage(allImages, allKeyWords) {
+  // Render all of the images
+
+  let $divEle = $('<div></div>');
+  allImages.forEach(img => {
+    renderWithHandleBars(img, $divEle);
+  });
+  $('main').append($divEle);
+  // Always hide the section template element
+  $('#photo-template').hide();
+
+  // Poplulate dropdown with keywords
+  populateDropDown(allKeyWords);
 }
 
 // Use jQuery to create a HTML photo element
 // --------------------------------------------------
-function renderWithJquery(imgObject) {
+function renderWithJquery(imgObject, $parentEle) {
   const $newElement = $('<section></section>');
   const newImgTemplate = $('#photo-template').html();
 
@@ -55,14 +62,15 @@ function renderWithJquery(imgObject) {
   $newElement.find('img').attr('src', imgObject.image_url);
   $newElement.find('p').text(imgObject.description);
 
-  $('main').append($newElement);
+  $parentEle.append($newElement);
 }
 
 // Populate the select dropdown with keywords
 // --------------------------------------------------
-function populateDropDown() {
+function populateDropDown(allKeyWords) {
   const $dropdown = $('select');
-
+  $dropdown.empty();
+  $dropdown.append($('<option>', {value: 'default', text: 'Filter by Keyword'}));
   allKeyWords.forEach(keyword => {
     $dropdown.append($('<option>', { value: keyword, text: keyword }));
   });
@@ -88,9 +96,59 @@ $('select').on('change', function(){
   $('#photo-template').hide();
 });
 
+$('#pagination').on('click', function(event){
+  event.preventDefault();
+  let page = $(event.target).html();
+  //console.log(page);
+  if(page === 'Page 1'){
+    $('main > div').hide();
+    $('main div:nth-child(2)').show();
+    populateDropDown(allKeyWordsPg1);
+  }
+  if(page === 'Page 2'){
+    $('main > div').hide();
+    $('main div:nth-child(3)').show();
+    populateDropDown(allKeyWordsPg2);
+  }
+});
+
+
+
+
+
+function renderWithHandleBars(imgObject, $parentEle) {
+  const source   = document.getElementById('img-template').innerHTML;
+  const template = Handlebars.compile(source);
+
+  const context = {
+    image_url: imgObject.image_url,
+    name: imgObject.name,
+    description: imgObject.description
+  };
+
+  const html = template(context);
+
+  $parentEle.append(html);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Run on Ready
 // --------------------------------------------------
 $(document).ready(function() {
   // Read JSON data from local data directory
-  readJSON('./data/page-1.json', 'json');
+  readJSON('./data/page-1.json', 'json', allImagesPg1, allKeyWordsPg1);
+  readJSON('./data/page-2.json', 'json', allImagesPg2, allKeyWordsPg2);
 });
